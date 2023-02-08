bcc1a3f0e45550f572f025272accd83db1ea9f55
red13xxx <31795640+red13xxx@users.noreply.github.com>
2023-02-08 13:18:28 +0100

    Ausgaben

--- Flower.gd
+++ Flower.gd
@@ -11,7 +11,6 @@ func _process(delta):
 	var bodies = self.get_colliding_bodies()
 	
 	if bodies.size() > 0:
-		print_debug(bodies[0].get_class())
 		if bodies[0].has_method("hit"):
 			bodies[0].hit()
 		
